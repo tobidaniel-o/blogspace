4b89f7776cee49bdb86074a293f709ed1d09c248
Tobi Okunuga <tobi.daniel2018@gmail.com>
2021-08-28 22:55:33 -0400

    blogspace creation

--- app.js
+++ app.js
@@ -1,18 +1,70 @@
+// fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+//   .then((response) => response.json())
+//   .then((data) => display(data));
+
+// const blogList = document.querySelector("#blog-list");
+// const title = document.createElement("h4");
+
+// function display(data) {
+//   const fiveList = data.slice(0, 5);
+//   console.log(fiveList);
+
+//   for (let i = 0; i < fiveList.length; i++) {
+//     title.textContent = fiveList[i].title;
+//     blogList.appendChild(title);
+//     blogList.innerHTML += `${fiveList[i].body} <hr />`;
+//   }
+//   return blogList;
+// }
+
+let postsArray = [];
+
+function renderPosts() {
+  let html = "";
+  for (let post of postArray) {
+    html += `
+        <h3>${post.title}</h3>
+        <p>${post.body}</p>
+        <hr />
+      `;
+  }
+  document.getElementById("blog-list").innerHTML = html;
+}
+
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
   .then((response) => response.json())
-  .then((data) => display(data));
+  .then((data) => {
+    postArray = data.slice(0, 5);
+    renderPosts();
+  });
 
-const blogList = document.querySelector("#blog-list");
-const title = document.createElement('h4')
+const newPost = document.querySelector("#new-post");
 
-function display(data) {
-  const fiveList = data.slice(0, 5)
-  console.log(fiveList);
+const handleSubmit = (event) => {
+  event.preventDefault();
+  let postTitle = document.querySelector("#post-title").value;
+  let postBody = document.querySelector("#post-body").value;
 
-  for (let i = 0; i < fiveList.length; i++) {
-    title.textContent = fiveList[i].title
-    blogList.appendChild(title)
-    blogList.innerHTML += `<br /> ${fiveList[i].body}`;
-  }
-  return blogList;
-}
+  const data = {
+    title: postTitle,
+    body: postBody,
+  };
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+    .then((res) => res.json())
+    .then((post) => {
+      postArray.unshift(post);
+      renderPosts();
+      newPost.reset();
+    });
+};
+
+newPost.addEventListener("submit", handleSubmit);
